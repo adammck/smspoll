18d63a2895e0587347e1651866fb466bd7988b33
adammck <adam.mckaig@gmail.com>
2010-10-25 11:58:08 -0400

    don't explode when admin.py is imported multiple times.

--- iffcpoll/admin.py
+++ iffcpoll/admin.py
@@ -3,10 +3,16 @@
 
 
 from django.contrib import admin
+from django.contrib.admin import sites
 from . import models
 
+try:
+    admin.site.register(models.Governorate)
+    admin.site.register(models.District)
+    admin.site.register(models.Option)
+    admin.site.register(models.Vote)
 
-admin.site.register(models.Governorate)
-admin.site.register(models.District)
-admin.site.register(models.Option)
-admin.site.register(models.Vote)
+# catch exceptions, to allow this module to be imported multiple times
+# without exploding. (urls.py and nose both import it during testing.)
+except sites.AlreadyRegistered:
+    pass
